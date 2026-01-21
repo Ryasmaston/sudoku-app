0c6b8bd6e8745c3229fcfe1cbf3e451e7ad9b23f
Issa Khafaga <ikhafaga@live.com>
2026-01-21 13:39:02 +0000

    feat: add homescreen func

--- app/src/main/java/com/example/sudoku_app/MainActivity.kt
+++ app/src/main/java/com/example/sudoku_app/MainActivity.kt
@@ -4,8 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -29,7 +41,9 @@ fun App() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "home") {
         composable(route = "home") {
-            HomeScreen()
+            HomeScreen(onGameStart = {
+                navController.navigate("game")
+            })
         }
         composable(route = "game") {
             GameScreen()
@@ -39,17 +53,26 @@ fun App() {
 }
 
 @Composable
-fun  HomeScreen(){}
-
-@Composable
-fun GameScreen(){}
-
-@Preview (showSystemUi = true)
-@Composable
-fun PreviewHome(){
-    SudokuappTheme() {
-        HomeScreen()
+fun HomeScreen(modifier: Modifier = Modifier, onGameStart: () -> Unit) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row() {
+            Text(stringResource(R.string.homepage_heading))
+        }
+        Spacer(modifier = modifier.height(16.dp))
+        Row() {
+            Button(onClick = onGameStart) {
+                Text(stringResource(R.string.start_game_btn))
+            }
+        }
     }
+}
+
+@Composable
+fun GameScreen() {
 }
 
 
